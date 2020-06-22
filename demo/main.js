66e247323e5cbf4738c7f97f79ff8da6b1eddb4c
Zach Posten <zach@posten.io>
2020-06-22 00:53:09 -0500

    chore: Recreate demo (lowercase) with same contents

---
+++ demo/main.js
@@ -0,0 +1,28 @@
+import {TintedWheel, TintedPalette} from 'tinted'
+
+let colorWheel = new TintedWheel({
+  colorWheelImage: './wheel.png',
+  container: '#tinted',
+})
+
+new TintedPalette(colorWheel)
+createModeToggle(colorWheel)
+
+colorWheel.bindData()
+
+function createModeToggle(colorWheel) {
+  let modeToggle = colorWheel.container
+    .append('select')
+    .attr('class', 'color-mode-toggle')
+    .on('change', function () {
+      colorWheel.currentMode = this.value
+      colorWheel.setHarmony()
+    })
+
+  for (let mode in TintedWheel.MODES) {
+    modeToggle
+      .append('option')
+      .text(TintedWheel.MODES[mode])
+      .attr('selected', () => (colorWheel.isInMode(mode) ? 'selected' : null))
+  }
+}
---
+++ demo/webpack.config.js
@@ -0,0 +1,36 @@
+const path = require('path')
+
+module.exports = {
+  mode: 'development',
+  entry: ['./main.js', './index.scss'],
+  output: {
+    path: __dirname,
+    filename: 'bundle.js',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.scss$/i,
+        use: [
+          // 3. Creates `style` nodes from CommonJS strings
+          {loader: 'style-loader'},
+          // 2. Translates CSS into CommonJS strings
+          {loader: 'css-loader'},
+          // 1. Compiles Sass to CSS
+          {
+            loader: 'sass-loader',
+            options: {
+              implementation: require('sass'),
+              sassOptions: {
+                includePaths: ['../node_modules'], // https://github.com/sass/node-sass#includepaths
+              },
+            },
+          },
+        ],
+      },
+    ],
+  },
+  // devServer: {
+  //   contentBase: path.join(__dirname, 'dist'),
+  // },
+}
